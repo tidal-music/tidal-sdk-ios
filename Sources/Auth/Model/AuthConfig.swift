a6d90c9cc4e03a873cb6e50eb27d14aa70aeb5dc
Evgenii Kononenko <ekononenko@tidal.com>
2024-05-16 12:24:59 +0200

    replaced Scopes with Set in AuthConfig

--- Sources/Auth/Model/AuthConfig.swift
+++ Sources/Auth/Model/AuthConfig.swift
@@ -6,7 +6,7 @@ public struct AuthConfig {
 	public let clientSecret: String?
 	public let credentialsKey: String
 	public let credentialsAccessGroup: String?
-	public let scopes: Scopes
+	public let scopes: Set<String>
 	public let tidalLoginServiceBaseUri: String
 	public let tidalAuthServiceBaseUri: String
 	public let enableCertificatePinning: Bool
@@ -17,7 +17,7 @@ public struct AuthConfig {
 		clientSecret: String? = nil,
 		credentialsKey: String,
 		credentialsAccessGroup: String? = nil,
-		scopes: Scopes = Scopes(),
+		scopes: Set<String> = [],
 		tidalLoginServiceBaseUri: String = "https://login.tidal.com",
 		tidalAuthServiceBaseUri: String = "https://auth.tidal.com",
 		enableCertificatePinning: Bool = true
