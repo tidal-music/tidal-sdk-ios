b552e545e391285f28e379cf67d2b3128a74db04
Alberto Sendra <asendra@squareup.com>
2024-05-28 13:24:07 +0200

    Use absolute URLs in documentation links

--- Scripts/combineDocumentation.sh
+++ Scripts/combineDocumentation.sh
@@ -5,13 +5,13 @@ mkdir -p docs/
 for target in "$@"
 do
     echo "Generating docs for $target"
-    swift package --allow-writing-to-directory "$target-docs" generate-documentation --disable-indexing --transform-for-static-hosting --output-path "$target-docs" --target "$target"
+    swift package --allow-writing-to-directory "$target-docs" generate-documentation --disable-indexing --transform-for-static-hosting --hosting-base-path tidal-sdk-ios --output-path "$target-docs" --target "$target"
     cp -r $target-docs/* docs/
     modified_target=$(echo $target | tr '-' '_' | tr '[:upper:]' '[:lower:]')
     cp -r $target-docs/index/index.json "docs/index/$modified_target.json"
 done
 
-echo "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url=/documentation/\" /></head><body><ol>" > docs/index.html
+echo "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url=/tidal-sdk-ios/documentation/\" /></head><body><ol>" > docs/index.html
 
 for target in "$@"
 do
@@ -19,7 +19,7 @@ do
     cp -R $target-docs/documentation/* docs/documentation/
     rm -r "$target-docs"
     modified_target=$(echo $target | tr '-' '_' | tr '[:upper:]' '[:lower:]')
-    echo "<li><a href=\"/documentation/$modified_target/\">$target</a></li>" >> docs/index.html
+    echo "<li><a href=\"/tidal-sdk-ios/documentation/$modified_target/\">$target</a></li>" >> docs/index.html
 done
 
 echo "</ol></body></html>" >> docs/index.html
