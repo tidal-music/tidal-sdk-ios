dfd65dd91baff66c832641739878b236b85ff3a5
Alberto Sendra <asendra@squareup.com>
2024-05-31 15:27:16 +0200

    Fixed formatting

--- Sources/Player/PlaybackEngine/Internal/Events/AsyncScheduler/TaskAsyncSchedulerFactory.swift
+++ Sources/Player/PlaybackEngine/Internal/Events/AsyncScheduler/TaskAsyncSchedulerFactory.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 final class TaskAsyncSchedulerFactory: AsyncSchedulerFactory {
-  @discardableResult
-  func create(code: @escaping () async -> Void) -> AsyncScheduler {
-	Task<Void, Never>.schedule(code: code)
-  }
+	@discardableResult
+	func create(code: @escaping () async -> Void) -> AsyncScheduler {
+		Task<Void, Never>.schedule(code: code)
+	}
 }
