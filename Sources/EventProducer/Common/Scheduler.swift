e83fa9be6a01ab928a935f5bb97b3c1617bcf8be
Evgenii Kononenko <ekononenko@tidal.com>
2024-07-03 18:50:20 +0200

    comment to Scheduler

--- Sources/EventProducer/Common/Scheduler.swift
+++ Sources/EventProducer/Common/Scheduler.swift
@@ -7,6 +7,10 @@ protocol Scheduler: AnyObject {
 }
 
 extension Scheduler {
+	/// The Scheduler applies a best-effort approach to sending events to the TL Consumer, meaning that it continuously tries to send
+	/// events while the app is executing,
+	/// regardless of current internet conditions or if the app considers itself to be online or not. As long as the app is
+	/// executing, the Scheduler shall be running
 	func runScheduling(with headerHelper: HeaderHelper) {
 		guard schedulerTask == nil else {
 			return
