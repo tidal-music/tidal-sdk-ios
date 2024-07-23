c8cde10fb837e9cdd5caf50fef7a9bcec7dd82e5
Evgenii Kononenko <ekononenko@tidal.com>
2024-07-23 16:06:39 +0200

    Loggable extension - for default values

--- Sources/Common/Logging/Loggable.swift
+++ Sources/Common/Logging/Loggable.swift
@@ -4,3 +4,13 @@ public protocol Loggable {
 	var message: String { get }
 	var level: LogLevel { get }
 }
+
+public extension Loggable {
+	var message: String {
+		return "Loggable message: \(self)"
+	}
+	
+	var level: LogLevel {
+		return .info
+	}
+}
