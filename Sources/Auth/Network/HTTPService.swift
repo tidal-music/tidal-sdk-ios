36991643c184876a1a6c32ffdc08dc3f74e8fb3a
Evgenii Kononenko <ekononenko@tidal.com>
2024-07-24 17:37:29 +0200

    HTTPService - log server error

--- Sources/Auth/Network/HTTPService.swift
+++ Sources/Auth/Network/HTTPService.swift
@@ -4,7 +4,7 @@ import Foundation
 // MARK: - HTTPService
 
 protocol HTTPService {
-	func executeRequest<T: Decodable>(_ request: URLRequest) async throws -> T
+	func executeRequest<T: Decodable>(_ request: URLRequest, logServerError: ((Error) -> Void)?) async throws -> T
 	func request(url: String, path: String, httpMethod: HTTPMethod, contentType: String, queryItems: [URLQueryItem])
 		-> URLRequest?
 }
@@ -32,7 +32,7 @@ extension HTTPService {
 		return request
 	}
 
-	func executeRequest<T: Decodable>(_ request: URLRequest) async throws -> T {
+	func executeRequest<T: Decodable>(_ request: URLRequest, logServerError: ((Error) -> Void)?) async throws -> T {
 		// Send the request asynchronously
 		let (data, response) = try await URLSession.shared.data(for: request)
 
@@ -40,9 +40,16 @@ extension HTTPService {
 		guard let httpResponse = response as? HTTPURLResponse, (200 ... 299).contains(httpResponse.statusCode) else {
 			let errorResponse = try? JSONDecoder().decode(ErrorResponse.self, from: data)
 			let statusCode = errorResponse?.status ?? (response as? HTTPURLResponse)?.statusCode ?? 1
-			throw NetworkError(code: "\(statusCode)", subStatus: errorResponse?.subStatus)
+			let errorMessage = "Error response: \(String(describing: errorResponse)), status code: \(statusCode), request: \(request)"
+			let error = NetworkError(code: "\(statusCode)", subStatus: errorResponse?.subStatus, message: errorMessage)
+			logServerError?(error)
+			throw error
 		}
 
 		return try JSONDecoder().decode(T.self, from: data)
+	}
+	
+	func executeRequest<T: Decodable>(_ request: URLRequest) async throws -> T {
+		try await executeRequest(request, logServerError: nil)
 	}
 }
