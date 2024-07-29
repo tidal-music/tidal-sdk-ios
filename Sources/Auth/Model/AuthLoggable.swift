e5402320c2ec4f0764b75b39300a0416aa3623f7
Evgenii Kononenko <ekononenko@tidal.com>
2024-07-29 20:37:43 +0200

    AuthLoggable - ability to log with swift-log

--- Sources/Auth/Model/AuthLoggable.swift
+++ Sources/Auth/Model/AuthLoggable.swift
@@ -1,4 +1,5 @@
 import Common
+import Logging
 import Foundation
 
 public enum AuthLoggable: Loggable {
@@ -37,5 +38,44 @@ public enum AuthLoggable: Loggable {
 	private func errorMessagePart(error: Error?) -> String {
 		guard let error = error else { return "" }
 		return ", error: \(error.localizedDescription)"
+	}
+}
+
+extension AuthLoggable {
+	private static let metadataErrorKey = "error"
+	private static let metadataReasonKey = "reason"
+	
+	var loggingMessage: Logging.Logger.Message {
+		"\(self)"
+	}
+	
+	var loggingMetadata: Logging.Logger.Metadata {
+		var metadata = [String: Logging.Logger.MetadataValue]()
+		
+		switch self {
+		case .initializeDeviceLoginNetworkError(let error),
+			 .finalizeLoginNetworkError(let error),
+			 .finalizeDeviceLoginNetworkError(let error),
+			 .getCredentialsUpgradeTokenNetworkError(let error),
+			 .getCredentialsRefreshTokenNetworkError(let error),
+			 .getCredentialsRefreshTokenWithClientCredentialsNetworkError(let error):
+			metadata[Self.metadataErrorKey] = .string(error.localizedDescription)
+		case .authLogout(let reason, let error):
+			metadata[Self.metadataReasonKey] = .string(reason)
+			if let error = error {
+				metadata[Self.metadataErrorKey] = .string(error.localizedDescription)
+			}
+			return metadata
+		default:
+			return [:]
+		}
+		
+		return metadata
+	}
+	
+	func log() {
+		var logger = Logging.Logger(label: "auth_logger")
+		logger.logLevel = .trace
+		logger.log(level: .error, loggingMessage, metadata: loggingMetadata)
 	}
 }
