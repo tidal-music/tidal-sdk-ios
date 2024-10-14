6f289afc2f5124fd63a033c0faf6a0650267932d
André Louw <andrelouw@squareup.com>
2024-10-14 09:45:28 +0200

    Add AnyCodableDictionaryConvertible

--- Sources/Common/AnyCodableDictionary.swift
+++ Sources/Common/AnyCodableDictionary.swift
@@ -1,3 +1,17 @@
 import AnyCodable
+import Foundation
 
 public typealias AnyCodableDictionary = [String: AnyCodable?]
+
+// MARK: - AnyCodableDictionaryConvertible
+
+public protocol AnyCodableDictionaryConvertible: Codable {
+	func asAnyCodableDictionary() throws -> AnyCodableDictionary
+}
+
+public extension AnyCodableDictionaryConvertible {
+	func asAnyCodableDictionary() throws -> AnyCodableDictionary {
+		let dictionary = try JSONEncoder().encode(self)
+		return try JSONDecoder().decode(AnyCodableDictionary.self, from: dictionary)
+	}
+}
--- Tests/CommonTests/CommonTests.swift
+++ Tests/CommonTests/CommonTests.swift
@@ -1,13 +1,48 @@
+import AnyCodable
 @testable import Common
+import Foundation
 import XCTest
 
-final class CommonTests: XCTestCase {
-	func testCommon() throws {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct
-		// results.
-		let common = Common()
+// MARK: - CommonTests
 
-		XCTAssertEqual(common.sayHello(), "Hello, World!")
+final class CommonTests: XCTestCase {
+	func test_asAnyCodableDictionary_convertsToAnyCodableDictionary() throws {
+		let codableFixture = CodableFixture.anyCodableFixture
+
+		let codableDictionary = try codableFixture.asAnyCodableDictionary()
+		let encodedDictionary = try JSONEncoder().encode(codableDictionary)
+		let decodedDictionary = try JSONDecoder().decode(CodableFixture.self, from: encodedDictionary)
+
+		XCTAssertEqual(codableFixture, decodedDictionary)
 	}
+}
+
+// MARK: - CodableFixture
+
+private struct CodableFixture: AnyCodableDictionaryConvertible, Equatable {
+	let optional: String?
+	let optionalString: String?
+	let string: String
+	let int: Int
+	let bool: Bool
+	let double: Double
+	let array: [String]
+	let dictionary: [String: String]
+	let innerStruct: InnerCodableStruct
+
+	struct InnerCodableStruct: Codable, Equatable {
+		let name: String
+	}
+
+	static let anyCodableFixture = CodableFixture(
+		optional: nil,
+		optionalString: "optionalStringValue",
+		string: "stringValue",
+		int: 1,
+		bool: true,
+		double: 1.2,
+		array: ["a", "b"],
+		dictionary: ["key": "value"],
+		innerStruct: .init(name: "Some-name")
+	)
 }
