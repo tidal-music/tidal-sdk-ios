c1feea2e7ae67f9ba563194eb9d77b6e53ab230a
Evgenii Kononenko <ekononenko@tidal.com>
2024-11-11 17:20:28 +0100

    extracted most common metadata keys to its extension

--- Sources/Common/Logger/LoggerMetadata+Extension.swift
+++ Sources/Common/Logger/LoggerMetadata+Extension.swift
@@ -5,4 +5,9 @@ public extension Logger.Metadata {
 	init(stringDict: [String: String]) {
 		self = stringDict.mapValues { .string($0) }
 	}
+	
+	// Common keys that can be used in metadata for the sake of consistency
+	static let errorKey = "error"
+	static let codeKey = "code"
+	static let reasonKey = "reason"
 }
