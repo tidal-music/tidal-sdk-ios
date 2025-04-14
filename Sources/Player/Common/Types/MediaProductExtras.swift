92eadd17b79fe47c070152a96f132d49223166b3
Alberto Sendra <asendra@squareup.com>
2025-04-14 16:04:08 +0200

    Add extra data to StreamingSessionStart for performance monitoring

--- Sources/Player/Common/Types/MediaProductExtras.swift
+++ Sources/Player/Common/Types/MediaProductExtras.swift
@@ -6,6 +6,12 @@ public extension MediaProduct {
 	typealias Extras = AnyCodableDictionary
 }
 
+extension MediaProduct.Extras {
+	enum Constants {
+		static let UploadsDictKey = "extras"
+	}
+}
+	
 public extension Encodable {
 	func asMediaProductExtras() throws -> MediaProduct.Extras {
 		try asAnyCodableDictionary()
