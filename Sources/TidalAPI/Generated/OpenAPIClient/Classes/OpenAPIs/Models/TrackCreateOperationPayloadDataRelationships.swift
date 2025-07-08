44fad5c809b5ef9e569d29fc40e23af273679300
TIDAL Music Tools <svc-github-tidal-music-tools@block.xyz>
2025-07-08 07:16:24 +0000

    Changed 33 files via GitHub Actions

--- Sources/TidalAPI/Generated/OpenAPIClient/Classes/OpenAPIs/Models/TrackCreateOperationPayloadDataRelationships.swift
+++ Sources/TidalAPI/Generated/OpenAPIClient/Classes/OpenAPIs/Models/TrackCreateOperationPayloadDataRelationships.swift
@@ -13,15 +13,19 @@ import AnyCodable
 public struct TrackCreateOperationPayloadDataRelationships: Codable, Hashable {
 
     public var albums: TrackCreateOperationPayloadDataRelationshipsAlbums
+    public var artists: TrackCreateOperationPayloadDataRelationshipsArtists
 
     public init(
-        albums: TrackCreateOperationPayloadDataRelationshipsAlbums
+        albums: TrackCreateOperationPayloadDataRelationshipsAlbums,
+        artists: TrackCreateOperationPayloadDataRelationshipsArtists
     ) {
         self.albums = albums
+        self.artists = artists
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case albums
+        case artists
     }
 
     // Encodable protocol methods
@@ -29,6 +33,7 @@ public struct TrackCreateOperationPayloadDataRelationships: Codable, Hashable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(albums, forKey: .albums)
+        try container.encode(artists, forKey: .artists)
     }
 }
 
---
+++ Sources/TidalAPI/Generated/docs/TrackStatisticsResource.md
@@ -0,0 +1,13 @@
+# TrackStatisticsResource
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**id** | **String** | resource unique identifier | 
+**type** | **String** | resource unique type | 
+**attributes** | [**TrackStatisticsAttributes**](TrackStatisticsAttributes.md) |  | [optional] 
+**relationships** | [**TrackStatisticsRelationships**](TrackStatisticsRelationships.md) |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
