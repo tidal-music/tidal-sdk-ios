2af3822abd20d4bd00fee8e5446e29b39185630f
Alberto Sendra <asendra@squareup.com>
2025-10-13 14:39:58 +0200

    Player: consolidate ABR monitoring

--- Sources/Player/PlaybackEngine/Internal/AVQueuePlayer/AVPlayerItemMonitor.swift
+++ Sources/Player/PlaybackEngine/Internal/AVQueuePlayer/AVPlayerItemMonitor.swift
@@ -2,27 +2,48 @@ import AVFoundation
 import Foundation
 
 final class AVPlayerItemMonitor {
+	private let playerItem: AVPlayerItem
 	private let failureMonitor: FailureMonitor
 	private let failedToPlayToEndMonitor: FailedToPlayToEndMonitor
 	private let stallMonitor: StallMonitor
 	private let completelyDownloadedMonitor: CompletelyDownloadedMonitor
 	private let readyToPlayMonitor: ReadyToPlayMonitor
 	private let playerItemDidPlayToEndTimeMonitor: ItemPlayedToEndMonitor
+	private var abrMonitor: AVPlayerItemABRMonitor?
 
 	init(
 		_ playerItem: AVPlayerItem,
+		queue: OperationQueue,
+		adaptiveQualities: [AudioQuality]?,
 		onFailure: @escaping (AVPlayerItem, Error?) -> Void,
 		onStall: @escaping (AVPlayerItem) -> Void,
 		onCompletelyDownloaded: @escaping (AVPlayerItem) -> Void,
 		onReadyToPlayToPlay: @escaping (AVPlayerItem) -> Void,
-		onItemPlayedToEnd: @escaping (AVPlayerItem) -> Void
+		onItemPlayedToEnd: @escaping (AVPlayerItem) -> Void,
+		onAudioQualityChanged: ((AVPlayerItem, AudioQuality) -> Void)?
 	) {
+		self.playerItem = playerItem
 		failureMonitor = FailureMonitor(playerItem, onFailure)
 		failedToPlayToEndMonitor = FailedToPlayToEndMonitor(playerItem, onFailure)
 		stallMonitor = StallMonitor(playerItem, onStall)
 		completelyDownloadedMonitor = CompletelyDownloadedMonitor(playerItem, onCompletelyDownloaded)
 		readyToPlayMonitor = ReadyToPlayMonitor(playerItem, onReadyToPlayToPlay)
 		playerItemDidPlayToEndTimeMonitor = ItemPlayedToEndMonitor(playerItem: playerItem, onPlayedToEnd: onItemPlayedToEnd)
+
+		if let onAudioQualityChanged,
+		   let qualities = adaptiveQualities,
+		   qualities.count > 1 {
+			abrMonitor = AVPlayerItemABRMonitor(
+				playerItem: playerItem,
+				qualities: qualities,
+				queue: queue
+			) { [weak self] newQuality in
+				guard let self else {
+					return
+				}
+				onAudioQualityChanged(self.playerItem, newQuality)
+			}
+		}
 	}
 
 	func isCompletelyDownloaded() -> Bool {
