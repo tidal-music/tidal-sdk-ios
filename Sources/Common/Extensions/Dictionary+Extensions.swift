53c77c4125127e7ecc2aa9fb5e47b842f450905f
Even Bekkedal <evenbekkedal@squareup.com>
2026-09-16 13:06:08 +0200

    TM-2038: Use failable String(data:encoding:) in Dictionary.jsonEncoded

--- Sources/Common/Extensions/Dictionary+Extensions.swift
+++ Sources/Common/Extensions/Dictionary+Extensions.swift
@@ -5,6 +5,6 @@ extension [String: String] {
 		guard let data = try? JSONEncoder().encode(self) else {
 			return nil
 		}
-		return String(decoding: data, as: UTF8.self)
+		return String(data: data, encoding: .utf8)
 	}
 }
